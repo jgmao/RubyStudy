ff8354df81a30de41cb68b8fe830d3a6a24f4929
Guoxin Jin <jgmao@hotmail.com>
2013-03-14 22:17:33 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailStudy::Application.config.secret_token = '4cc4277bd78be5c1443d1a28b3d78fbe796229b47e0dc15660e6ead90355214ce4df9bdec0944d9e05435a86de44f400800d63843c6ef246be2a9da40d4aebf2'
